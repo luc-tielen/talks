806a3a5e6c5d9837fad6d68bddaf32f1a783dece
Luc Tielen <luc.tielen@kabisa.nl>
2018-11-19 13:36:59 +0100

    Add check to see if port is already in use (by other presentation)

--- bin/start_presentation.js
+++ bin/start_presentation.js
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process");
+const tcpPortUsed = require("tcp-port-used");
 
-const folderName = process.argv[2];
-if (!folderName) {
-  process.stderr.write(
-    "You need to specify a folder to start presenting from!\n"
-  );
-  process.exit(1);
-}
+const HOST = "127.0.0.1";
+const PORT = 8080;
 
-execSync(`yarn mdx-deck ${folderName}/presentation.mdx`, { stdio: "inherit" });
-process.exit(0);
+const main = async () => {
+  const folderName = process.argv[2];
+  if (!folderName) {
+    process.stderr.write(
+      "You need to specify a folder to start presenting from!\n"
+    );
+    process.exit(1);
+  }
+
+  const isPortInUse = await tcpPortUsed.check(PORT, HOST);
+  if (isPortInUse) {
+    process.stderr.write(`Port ${PORT} is already in use, aborting.\n`);
+    process.exit(1);
+  }
+
+  execSync(`yarn mdx-deck ${folderName}/presentation.mdx`, {
+    stdio: "inherit"
+  });
+  process.exit(0);
+};
+
+main();
