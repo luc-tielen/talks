36c977acb8030b5cb7cb3074b7bef63968774a9d
Luc Tielen <luc.tielen@kabisa.nl>
2019-10-12 11:27:44 +0200

    Cleanup deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,37 +12,64 @@ DIRS=(
   haskell/functors
 )
 
-# Hack for being able to do static builds with mdx-deck.
-FILE=node_modules/mdx-deck/lib/html.js
-sed -i "s/'mdx-deck\/layouts']/'mdx-deck\/layouts', 'mdx-deck-code-surfer', 'mdx-deck-kabisa-theme']/g" $FILE
+function setup_node_modules() {
+  # Hack for being able to do static builds with mdx-deck.
+  FILE=node_modules/mdx-deck/lib/html.js
+  sed -i "s/'mdx-deck\/layouts']/'mdx-deck\/layouts', 'mdx-deck-code-surfer', 'mdx-deck-kabisa-theme']/g" $FILE
+}
 
-git stash > /dev/null
+function setup_working_directory() {
+  git stash > /dev/null
+  echo "Cleaning dist folder..."
+  rm -rf dist/
+}
 
-echo "Cleaning dist folder..."
-rm -rf dist/
-
-
-for DIR in "${DIRS[@]}"; do
+function build_talk() {
+  DIR=$1
   echo "Building presentation: $DIR..."
   yarn mdx-deck build --no-html --webpack webpack.config.js $DIR/presentation.mdx
+}
+
+function prepare_talk_for_deploy() {
   FILES=$(find dist/ -type f -depth 1)
   mkdir -p dist/$DIR
   mv $FILES dist/$DIR > /dev/null || echo "Finished building: $DIR..."
-done
+}
 
-echo "Preparing deploy..."
-git checkout gh-pages
-rm -rf "${DIRS[@]}"
-mv dist/* .
+function build_talks() {
+  for DIR in "${DIRS[@]}"; do
+    build_talk $DIR
+    prepare_talk_for_deploy $DIR
+  done
+}
 
-echo "Deploying to github pages..."
-git add "${DIRS[@]}"
-git commit -m 'Update talks'
-git push
+function prepare_deploy() {
+  echo "Preparing deploy..."
+  git checkout gh-pages
+  rm -rf "${DIRS[@]}"
+  mv dist/* .
+}
 
-echo "Finished deploy, cleaning up working directory..."
-git checkout -
-git stash pop > /dev/null || echo
+function deploy() {
+  echo "Deploying to github pages..."
+  git add "${DIRS[@]}"
+  git commit -m 'Update talks'
+  git push
+}
+
+function restore_working_directory() {
+  echo "Finished deploy, cleaning up working directory..."
+  git checkout -
+  git stash pop > /dev/null || echo
+}
+
+
+setup_node_modules
+setup_working_directory
+build_talks
+prepare_deploy
+deploy
+restore_working_directory
 
 echo "Done!"
 exit 0
