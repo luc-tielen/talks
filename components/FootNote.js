d7a45e9d6947ae677fa66b91a6b7c3c8e741060f
Luc Tielen <luc.tielen@kabisa.nl>
2020-11-05 20:45:19 +0100

    Add FootNote component

---
+++ components/FootNote.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+export const FootNotes = styled.div({
+  position: "absolute",
+  bottom: "10%",
+  width: "100%",
+  display: "flex",
+  justifyContent: "center"
+});
+
+export const FootNote = styled.p({
+  marginLeft: "-25%",
+  fontSize: "0.8em"
+});
